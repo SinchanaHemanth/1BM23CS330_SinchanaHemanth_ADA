e7e2e12c4ebaf5efca3b22652ff67605c0d8b557
SinchanaHemanth <sinchanahemanth.cs23@bmsce.ac.in>
2025-05-21 03:23:30 +0530

    Create leetcode2.c

---
+++ leetcode2.c
@@ -0,0 +1,29 @@
+int* asteroidCollision(int* asteroids, int asteroidsSize, int* returnSize) {
+     int* stack = (int*)malloc(asteroidsSize * sizeof(int));
+    int top = -1; 
+
+    for (int i = 0; i < asteroidsSize; i++) {
+        int asteroid = asteroids[i];
+        
+        while (top >= 0 && stack[top] > 0 && asteroid < 0) {
+            if (stack[top] < -asteroid) {
+                top--;
+                continue;
+            } else if (stack[top] == -asteroid) {
+                top--;
+                asteroid = 0;
+                break;
+            } else {
+                asteroid = 0;
+                break;
+            }
+        }
+        
+        if (asteroid != 0) {
+            stack[++top] = asteroid;
+        }
+    }
+
+    *returnSize = top + 1;
+    return stack;
+}
